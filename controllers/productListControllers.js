7ec28ddbef1c245a6f2265a055ef12bcf97cc6f4
lizhiwei <lizhiwei@qiniu.com>
2015-11-25 10:30:39 +0800

    学习angular

---
+++ controllers/productListControllers.js
@@ -0,0 +1,27 @@
+angular.module("sportsStore")
+.constant("productListActionClass","btn-primary")
+.constant("productListPageCount",3)
+.controller("productListCtrl",function($scope,$filter,productListActionClass,productListPageCount,cart){
+	var selectedCategory=null;
+	$scope.selectedPage=1;
+	$scope.pageSize=productListPageCount;
+	$scope.selectCategory=function(newCategory){
+		selectedCategory=newCategory;
+		$scope.selectedPage=1;
+	};
+	$scope.selectPage=function(newPage){
+		$scope.selectedPage=newPage;
+	};
+	$scope.categoryFilterFn=function(product){
+		return selectedCategory==null||product.category==selectedCategory;
+	};
+	$scope.getCategoryClass=function(category){
+		return selectedCategory==category?productListActionClass:"";
+	};
+	$scope.getPageClass=function(page){
+		return $scope.selectedPage==page?productListActionClass:"";
+	};
+	$scope.addProductToCart=function(product){
+		cart.addProduct(product.id,product.name,product.price);
+	}
+})
--- js/server.js
+++ js/server.js
@@ -1,5 +1,6 @@
 var connect=require('connect'),
 	  serverStatic=require('serve-static');
 var app=connect();
-app.use(serverStatic("../../upskills"));
+app.use(serverStatic("../upskills"));
+console.log(1);
 app.listen(5000);
