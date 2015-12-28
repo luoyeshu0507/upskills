c8469afa5be25062824e7e3a6497d64c30937e77
lizhiwei <lizhiwei@qiniu.com>
2015-12-28 16:52:55 +0800

    s

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,30 +1,62 @@
+//angular.module("sportsStore")
+//.constant("dataUrl","http://localhost:5555/products")
+//.constant("orderUrl","http://localhost:5555/order")
+//.controller("sportsStoreCtrl",function($scope,$http,$location,dataUrl,orderUrl,cart){
+//	$scope.data={};
+//	$http.get(dataUrl)
+//	.success(function(data){
+//		$scope.data.products=data;
+//	})
+//	.error(function(error){
+//		$scope.data.error=error;
+//	});
+//	
+//	$scope.sendOrder=function(shippingDetails){
+//		var order=angular.copy(shippingDetails);
+//		order.products=cart.getProducts();
+//		$http.post(orderUrl,order)
+//		.success(function(data){
+//			$scope.data.orderId=data.id;
+//			cart.getProduct().length=0;
+//		})
+//		.error(function(error){
+//			$scope.data.orderError=error;
+//		})
+//		.finally(function(){
+//			$location.path("/complete")
+//		})
+//	}
+//});
+//
 angular.module("sportsStore")
-.constant("dataUrl","http://localhost:5555/products")
-.constant("orderUrl","http://localhost:5555/order")
-.controller("sportsStoreCtrl",function($scope,$http,$location,dataUrl,orderUrl,cart){
-	$scope.data={};
-	$http.get(dataUrl)
-	.success(function(data){
-		$scope.data.products=data;
-	})
-	.error(function(error){
-		$scope.data.error=error;
-	});
-	
-	$scope.sendOrder=function(shippingDetails){
-		var order=angular.copy(shippingDetails);
-		order.products=cart.getProducts();
-		alert(order.products.length);
-		$http.post(orderUrl,order)
-		.success(function(data){
-			$scope.data.orderId=data.id;
-			cart.getProduct().length=0;
-		})
-		.error(function(error){
-			$scope.data.orderError=error;
-		})
-		.finally(function(){
-			$location.path("/complete")
-		})
-	}
-});
+    .constant("dataUrl", "http://localhost:5555/products")
+    .constant("orderUrl", "http://localhost:5555/order")
+    .controller("sportsStoreCtrl", function ($scope, $http, $location,
+        dataUrl, orderUrl, cart) {
+
+        $scope.data = {
+        };
+
+        $http.get(dataUrl)
+            .success(function (data) {
+                $scope.data.products = data;
+            })
+            .error(function (error) {
+                $scope.data.error = error;
+            });
+
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function (error) {
+                    $scope.data.orderError = error;
+                }).finally(function () {
+                    $location.path("/complete");
+                });
+        }
+    });
