2f047a0dd9e49d0b936353dd3b1e93eed6a2a4ed
lizhiwei <550822844@qq.com>
2016-08-11 14:41:00 +0800

    learn ts

---
+++ learntypescript/LettersOnlyValidator.js
@@ -0,0 +1,13 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var lettersRegexp = /^[A-Za-z]+$/;
+    var LettersOnlyValidator = (function () {
+        function LettersOnlyValidator() {
+        }
+        LettersOnlyValidator.prototype.isAcceptable = function (s) {
+            return lettersRegexp.test(s);
+        };
+        return LettersOnlyValidator;
+    }());
+    exports.LettersOnlyValidator = LettersOnlyValidator;
+});
