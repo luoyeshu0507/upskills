fffef47d3c336c71a71e7d65c8197e0b6122e9bc
lizhiwei <550822844@qq.com>
2016-10-31 23:42:14 +0800

    learn es6 set and map

---
+++ learn/es6/js/setAndMap.js
@@ -0,0 +1,6 @@
+"use strict";
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var set = new Set([1, 2, 3, 4, 5, 5]);
+console.log([].concat(_toConsumableArray(set)));
