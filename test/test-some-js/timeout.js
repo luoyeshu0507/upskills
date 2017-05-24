f9db38320fd4244f41a07898b7387148daff58cf
lizhiwei <550822844@qq.com>
2017-05-24 23:36:14 +0800

    time out

---
+++ test/test-some-js/timeout.js
@@ -0,0 +1,12 @@
+setTimeout(function() {
+	console.log(1);
+})
+setTimeout(function() {
+	console.log(11);
+})
+setTimeout(function() {
+	console.log(111);
+},2)
+setTimeout(function() {
+	console.log(1111);
+},1)
