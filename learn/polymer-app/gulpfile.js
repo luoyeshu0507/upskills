aa212dfb90482040bb18925d7fd957d320aacc23
luoyeshu0507 <550822844@qq.com>
2017-06-23 22:39:29 +0800

    add some components and refine build process

---
+++ learn/polymer-app/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync').create();
+var reload       = browserSync.reload;
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./public",
+      routes: {
+        "/bower_components": "bower_components"
+      },
+      middleware: function (req, res, next) {
+        res.setHeader('xxx', '123');
+        next();
+      }
+    },
+    port:3012
+  });
+
+  gulp.watch(['public/**/*.html', "!node_modules/**"]).on('change', reload);
+});
+
+gulp.task('default', ['serve']);
