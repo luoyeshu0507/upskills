b787dbc20d5128945e36fbb609858846c99cd737
luoyeshu0507 <550822844@qq.com>
2018-01-31 16:53:39 +0800

    learn parcel

--- learn/parcel-bundler/js/index.js
+++ learn/parcel-bundler/js/index.js
@@ -6,6 +6,14 @@ hello();
 import hello2 from './module2';
 hello2();
 
+import hello4 from './module4';
+
+new hello4().sayHello();
+
+import('./sub-module1').then((module) => {
+  module.hello5()
+})
+
 import '../css/test1.css';
 import '../css/test2.scss';
 
---
+++ learn/parcel-bundler/js/sub-module1.js
@@ -0,0 +1,5 @@
+const hello5 = function() {
+  console.log('hello5')
+};
+
+export { hello5 }
