13b388865475e5388a3bf79678253b6767c4c30c
luoyeshu0507 <550822844@qq.com>
2019-01-24 20:35:19 +0800

    deep object setvalue

---
+++ learn/deepObjectSetValue/index.js
@@ -0,0 +1,23 @@
+function deepSet(obj, key, val) {
+    console.log('before', obj);
+    if (getType(obj) === 'object' && getType(key) === 'string') {
+        var keyArr = key.split('.');
+        var cur = obj;
+        for (var i = 0; i < keyArr.length; i++) {
+            if (i === keyArr.length - 1) {
+                cur[keyArr[i]] = val;
+            } else {
+                cur = getType(cur[keyArr[i]]) === 'object'? cur[keyArr[i]]: cur[keyArr[i]] = {};
+            }
+        }
+    }
+    console.log('after', obj);
+    return obj;
+}
+
+function getType(val) {
+    return Object.prototype.toString.call(val).toLocaleLowerCase().match(/\w+(?=\])/)[0];
+}
+
+deepSet({}, 'a.b.c', 1);
+deepSet({a: {d: 2}}, 'a.b.c', 1);
